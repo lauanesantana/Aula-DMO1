0d506c7af894c59f4da53a2925be158e9e7cfad4
lauanesantana <lauanesantana60@gmail.com>
2026-08-03 10:28:03 -0300

    primeira atualizacao no teste2 em kotlin

---
+++ Teste2.kt
@@ -0,0 +1,14 @@
+class Teste2 {
+
+    fun main () {
+
+        println ("Hello World!!!!!!!!");
+        var num1 = 12;
+        val num2: Int = num1;
+        num1 = 55;
+
+        println (num1);
+        println (num2);
+
+    }
+ }
