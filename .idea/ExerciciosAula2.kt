3c14b96be0211316f1c1cdfb5805328f728058e0
lauanesantana <lauanesantana60@gmail.com>
2026-08-10 10:17:54 -0300

    exercicios iniciais + aula 2

---
+++ .idea/ExerciciosAula2.kt
@@ -0,0 +1,32 @@
+fun main () {
+
+    println ("Digite o seu nome: ");
+    val nome = readln();
+
+    println ("Digite a sua idade: ");
+    /* val idade = readln().toInt(); */
+    val idade = readln().toIntOrNull();
+
+    if (idade != null) {
+        println("Idade: $idade");
+    } else {
+        println ("Idade inválida");
+    }
+    val idade2 = 45
+    //semelhante ao switch, siginfica quando
+    val resultado = when {
+        idade2 >= 18 -> "Acabou de ficar com maioridade penal"
+        idade2 >= 65 -> "Terceira idade"
+        else -> println("Outra idade");
+    }
+    println ("Resultado: $resultado");
+
+/*    val dados = """
+        Nome: $nome,
+        /*Idade = $idade,*/
+        Idade ano que vem: ${idade+1}
+    """ .trimIndent()
+*/
+//    println(dados);
+}
+
